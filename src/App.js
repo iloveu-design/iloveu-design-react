7f966a61b6adf9ec510674b6d552613796b4663c
ahntae <ahntae@iloveu.design>
2021-08-31 15:58:05 +0900

    이미지 업로드

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
+import React from "react";
 
-function App() {
+
+function Project({company, siteUrl, parts, youtube_id}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>멋진 웹사이트를 만들도록 도와드립니다</h1>
-      </header>
+    <div>
+      <img src=""/>
+      <h2><a href={siteUrl} target="_blank" rel="noopener noreferrer">{company}</a></h2>
+      <span>{parts.map(current=>(current))}</span>
     </div>
   );
+}
+class App extends React.Component{
+
+  state = {
+    projects: [
+      {id: 1, company: '이그젝스디엑스', siteUrl: "http://exax-dx.com/", parts: ["frontend"], youtube: "", thumbnail: ""}
+    ]
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>멋진 웹사이트를 만들도록 도와드립니다.</h1>
+        {this.state.projects.map(project => (
+          <Project key={project.id} company={project.company} siteUrl={project.siteUrl} parts={project.parts}/>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
