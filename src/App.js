8f93a432e45c21e577772fc5c5f8cbd3c7c6930f
ahntae <ahntae@iloveu.design>
2021-08-31 18:52:25 +0900

    이그젝스디엑스 src 삽입

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 
-function Project({company, siteUrl, parts, youtube_id}) {
+function Project({company, siteUrl, parts, youtube_id, thumbnail}) {
   return (
     <div>
-      <img src=""/>
+      <img src={thumbnail} alt={company}/>
       <h2><a href={siteUrl} target="_blank" rel="noopener noreferrer">{company}</a></h2>
       <span>{parts.map(current=>(current))}</span>
     </div>
@@ -14,7 +14,7 @@ class App extends React.Component{
 
   state = {
     projects: [
-      {id: 1, company: '이그젝스디엑스', siteUrl: "http://exax-dx.com/", parts: ["frontend"], youtube: "", thumbnail: ""}
+      {id: 1, company: '이그젝스디엑스', siteUrl: "http://exax-dx.com/", parts: ["frontend"], youtube: "", thumbnail: "https://cdn.jsdelivr.net/gh/iloveu-design/iloveu-design-react/public/images/projects/exaxdx-og-image.jpg"}
     ]
   }
 
@@ -23,7 +23,7 @@ class App extends React.Component{
       <div>
         <h1>멋진 웹사이트를 만들도록 도와드립니다.</h1>
         {this.state.projects.map(project => (
-          <Project key={project.id} company={project.company} siteUrl={project.siteUrl} parts={project.parts}/>
+          <Project key={project.id} company={project.company} siteUrl={project.siteUrl} parts={project.parts} thumbnail={project.thumbnail}/>
         ))}
       </div>
     )
