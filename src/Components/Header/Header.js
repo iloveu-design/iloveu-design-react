78161666c6b4057f231a922810a0e85b20edf28d
ahntae <ahntae@iloveu.design>
2021-09-06 08:56:07 +0900

    fetch hide unused function

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
-import Home from "../../Routes/Home";
+
 const Header = styled.header`
     display: flex;
     border-bottom: thin solid #999;
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,18 +1,19 @@
 import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "../Routes/Home";
 import Projects from "../Routes/Projects";
-import Search from "../Routes/Search";
+// import Search from "../Routes/Search";
 import Header from "./Header";
 
-
-export default () => (
+const SRouter = () => (
     <Router>
         <Header/>
         <Switch>
             <Route path="/" exact component={Home}/>
             <Route path="/projects" component={Projects} />
-            <Route path="/search" component={Search} />
+            {/* <Route path="/search" component={Search} /> */}
             <Redirect from="*" to="/"/>
         </Switch>
     </Router>
 );
+
+export default SRouter;
